1022cbfb8eb4bc6609a37b4be676863c49e14902
Jake Doxey <jakedoxey@gmail.com>
2024-02-03 09:52:16 -0800

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Candidate johnDoe = new Candidate(10, "John", "Doe", "Republican");
         Utility.printCandidateInfo(johnDoe);
+        johnDoe.setName("John", "Bow");
+        Utility.printCandidateInfo(johnDoe);
+
         Voter janeDoe = new Voter(11, "Jane", "Doe",  true);
+        Utility.printVoterInfo(janeDoe);
+        janeDoe.setName("Jane", "Bow");
         Utility.printVoterInfo(janeDoe);
     }
 }
